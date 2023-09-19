371b59008de9cc8ceb71a15560ca63b43fc30756
Nerea Pinol <nereapinol@iesebre.com>
2023-09-19 13:28:17 +0200

    he afeguit el primer projecte java

---
+++ PrimerProjecte/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    // Exemple de comentari de una sola linea
+    public static void main(String[] args) {
+        /* Inici de comentari de mes d'una linea
+        System.out.println("Hello world!");
+    }  Fi de comentari de una linea*/
+//Vaig a mostrar per pantalla el missatge de"Hello World
+        /*...*/
+        System.out.println("Hello World");
+        System.out.println("Segona linea");
+        System.out.print("Me dic Nerea");
+        System.out.println("i sóc l'alumna de l'MP3");
+    }
+}
